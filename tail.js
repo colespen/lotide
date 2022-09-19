74df321a36ddc76b980f2b9858adb1003e23fe7a
colspen <spencerlewiscole@gmail.com>
2022-09-19 22:41:42 +0000

    slice off head of array and assert test

---
+++ tail.js
@@ -0,0 +1,19 @@
+const {assertEqual} = require('./assertEqual.js');
+
+const tail = (arr) => {
+  console.log(arr.slice(1));
+}
+
+// Test Case: check original array
+
+let words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+words = ["Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+words = [];
+tail(words);
+assertEqual(words.length, 3);
