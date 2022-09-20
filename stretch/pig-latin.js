001ada4de760cb1513ba10333bd4b64bd287d98a
colspen <spencerlewiscole@gmail.com>
2022-09-20 02:04:43 +0000

    pig latin function!

--- stretch/pig-latin.js
+++ stretch/pig-latin.js
@@ -1,4 +1,17 @@
 //function to converts CLI args to custom pig latin
-const pArgv = process.argv.slice(2);
+const argv = process.argv.slice(2);
 
+const pig = () => {
+  let pStr = "";
+  
+  for (let i = 0; i < argv.length; i++) {
+    for (let l = 1; l < argv[i].length; l++) {
+      pStr += argv[i][l];
+    }
+    pStr += argv[i][0] + "ay";
+    pStr += " ";
+  }
+  return pStr;
+};
 
+console.log(pig());
