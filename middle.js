ca11bd1c4a2a4c4ea580fa36ebba35a86ae0d002
colspen <spencerlewiscole@gmail.com>
2022-09-21 17:38:52 +0000

    working! ...still think assertion function isn't working as intended...

---
+++ middle.js
@@ -0,0 +1,51 @@
+const eqArrays = function(a, b) {
+  let result = true;
+  if (a.length !== b.length) {
+    result = false;
+  }
+  a.forEach((num1, i) => {
+    const num2 = b[i];
+    if (num1 !== num2) {
+      result = false;
+    }
+  });
+  return result;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ?
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const middle = function(array) {
+  array.splice(0, 1);
+  array.splice(-1, 1);
+  if (array.length % 2 === 0) {
+    while (array.length > 2) {
+      array.splice(0, 1);
+      array.splice(-1, 1);
+    }
+  } else if (array.length > 1) {
+    array.splice(0, 1);
+    array.splice(-1, 1);
+  }
+  return array;
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+middle([1]); // => []
+middle([1, 2]); // => []
+
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
