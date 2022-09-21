3c5d72abc8b70559cf110bc873ef748fee053c5e
colspen <spencerlewiscole@gmail.com>
2022-09-21 22:16:26 +0000

    function defined, preliminary test run

---
+++ eqObjects.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual(eqObjects(ab, ba), true); // => true
+assertEqual(eqObjects(ab, abc), false); // => false
