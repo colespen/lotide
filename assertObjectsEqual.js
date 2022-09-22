69a511ae215e20ea21997c6566db3d854fbc7a99
colspen <spencerlewiscole@gmail.com>
2022-09-22 00:11:40 +0000

    implemented

---
+++ assertObjectsEqual.js
@@ -0,0 +1,20 @@
+const eqObjects = function(obj1, obj2) {
+  const key1 = Object.keys(obj1);
+  const key2 = Object.keys(obj2);
+  if (key1.length !== key2.length) {
+    return false;
+  }
+  for (const key in obj1) {
+    if (Array.isArray(obj1[key] && obj2[key])) {
+      return eqArrays(obj1[key], obj2[key]);
+    }
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  
+}
