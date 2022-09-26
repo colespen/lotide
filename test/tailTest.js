9fd99b59185468b12f7885878f94d4a71a83ca4c
colspen <spencerlewiscole@gmail.com>
2022-09-26 19:25:13 +0000

    Mocha Chai update

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,54 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-let words = ["Yo Yo", "Lighthouse", "Labs"];
-const result1 = tail(words);
-assertEqual(result1.length, 2);
-assertEqual(result1[0], "Lighthouse");
-assertEqual(result1[1], "Labs");
+describe("tail", () => {
+  let words1 = ["Yo Yo", "Lighthouse", "Lab"];
+  const result1 = tail(words1);
 
-words = ["Lighthouse", "Labs"];
-const result2 = tail(words);
-assertEqual(result2.length, 1);
-assertEqual(result2[0], "Labs");
-assertEqual(result2[-1], undefined);
+  let words2 = ["Lighthouse", "Labs"];
+  const result2 = tail(words2);
 
-words = [];
-const result3 = tail(words);
-assertEqual(result3.length, 0);
+  let words3 = [];
+  const result3 = tail(words3);
+
+  it("returns 2 for ['Yo Yo', 'Lighthouse', 'Labs'] length", () => {
+    assert.strictEqual(result1.length, 2);
+  });
+  it("returns 'Lighthouse' for ['Yo Yo', 'Lighthouse', 'Labs'] index [0]", () => {
+    assert.strictEqual(result1[0], "Lighthouse");
+  });
+  it("returns 'Labs' for ['Yo Yo', 'Lighthouse', 'Labs'] index [1]", () => {
+    assert.strictEqual(result1[0], "Lighthouse");
+  });
+
+  it("returns 1 for ['Lighthouse', 'Labs'] length", () => {
+    assert.strictEqual(result2.length, 1);
+  });
+  it("returns 'Labs' for ['Lighthouse', 'Labs'] length index [0]", () => {
+    assert.strictEqual(result2[0], "Labs");
+  });
+  it("returns undefined for ['Lighthouse', 'Labs'] index [-1]", () => {
+    assert.strictEqual(result2[-1], undefined);
+  });
+
+  it("returns 0 for [].length", () => {
+    assert.strictEqual(result3.length, 0);
+  });
+});
+
+// let words = ["Yo Yo", "Lighthouse", "Labs"];
+// const result1 = tail(words);
+// assertEqual(result1.length, 2);
+// assertEqual(result1[0], "Lighthouse");
+// assertEqual(result1[1], "Labs");
+
+// let words2 = ["Lighthouse", "Labs"];
+// const result2 = tail(words2);
+// assertEqual(result2.length, 1);
+// assertEqual(result2[0], "Labs");
+// assertEqual(result2[-1], undefined);
+
+// let words3 = [];
+// const result3 = tail(words3);
+// assertEqual(result3.length, 0);
