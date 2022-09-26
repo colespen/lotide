7a0100580c2d132d4874007ea3b17edcd976b829
colspen <spencerlewiscole@gmail.com>
2022-09-26 14:03:41 +0000

    moved test code and export/require

--- tail.js
+++ tail.js
@@ -1,23 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const tail = (arr) => {
   return arr.slice(1);
 };
 
-
-// Test Case: check original array
-let words = ["Yo Yo", "Lighthouse", "Labs"];
-const result1 = tail(words);
-assertEqual(result1.length, 2);
-assertEqual(result1[0], "Lighthouse");
-assertEqual(result1[1], "Labs");
-
-words = ["Lighthouse", "Labs"];
-const result2 = tail(words);
-assertEqual(result2.length, 1);
-assertEqual(result2[0], "Labs");
-assertEqual(result2[-1], undefined);
-
-words = [];
-const result3 = tail(words);
-assertEqual(result3.length, 0);
+module.exports = tail;
